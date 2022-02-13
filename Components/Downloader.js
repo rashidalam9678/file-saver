00752d06c78c2e02b4b6d6eb0233c3abe7fe5c24
rashidalam9678 <mohdrashidalam786@gmail.com>
2022-02-13 23:06:01 +0530

    simple image downloader

---
+++ Components/Downloader.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Axios from 'axios'
+import FileDownload from 'js-file-download'
+import styles from '../styles/Downloader.module.css'
+
+
+
+
+
+const Downloader = (props) => {
+
+    const downloadfile = () => {
+        Axios({
+          url:(props.url),
+          method:"GET",
+          responseType:"blob"
+        }).then((res)=>{
+          FileDownload(res.data, 'downloaded_img.png')
+        })
+    }
+  return (
+    <div className={styles.Downloader}>
+        <button onClick={(e)=>downloadfile()}>
+            Download
+        </button>
+    </div>
+  )
+}
+
+export default Downloader
---
+++ firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTPy43gOugsYnklIzMXQQKZoqi2O4NQFI",
+  authDomain: "image-downloader-15b9f.firebaseapp.com",
+  projectId: "image-downloader-15b9f",
+  storageBucket: "image-downloader-15b9f.appspot.com",
+  messagingSenderId: "451278600051",
+  appId: "1:451278600051:web:1259c4f5e5e00322817706"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const projectStorage = getStorage(app);
+const projectFirestore = getFirestore(app);
+export { projectStorage, projectFirestore};
